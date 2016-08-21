864762fcf0074ee2524e61e80ee52519e2dff00d
watch1k <host.sf@gmail.com>
2016-08-21 14:41:34 +0300

    upd

--- build/js/app.js
+++ build/js/app.js
@@ -163,89 +163,86 @@ $(document).ready(function () {
     });
   }());
 
-  $('.one').fullpage({
-    navigation: true,
-    navigationPosition: 'left',
-    scrollOverflow: true,
-    normalScrollElements: '.navigation',
-    afterLoad: function (anchorLink, index) {
-      //fix IE svg text
-      $('body').css('font-size', '16px');
-      $('body').attr({
-        style: 'font-size: 16px;'
-      });
-      if ($(window).width() <= 1024) {
-        $('.js-svg-size').attr('y', '42%');
-        $('.js-svg-size-2').attr('y', '58%');
-
-        $.fn.fullpage.destroy('all');
-      }
-    },
-    afterRender: function () {
-      $('.js-video-1').get(0).play();
-    },
-    onLeave: function (index, nextIndex, direction) {
-      var header = $('.js-header'),
-        svgMask = $('.js-svg-mask'),
-        svgOverlay = $('.js-svg-overlay'),
-        navigation = $('.js-navigation'),
-        nav = $('#fp-nav'),
-        footer = $('.js-footer'),
-        hamburgerBtn = $('.js-hamburger');
-
-      if (index == 1) {
-        svgMask.addClass('is-active');
-        header.addClass('is-active');
-        svgOverlay.addClass('is-active');
-        hamburgerBtn.fadeIn();
-        navigation.hide().addClass('is-mod');
-        nav.addClass('is-active');
-        footer.addClass('is-hidden');
-      }
-      if (nextIndex == 1) {
-        svgMask.removeClass('is-active');
-        header.removeClass('is-active');
-        svgOverlay.removeClass('is-active');
-        hamburgerBtn.fadeOut();
-        navigation.show().removeClass('is-mod');
-        nav.removeClass('is-active');
-        footer.removeClass('is-hidden');
-      }
-
-      if (index < 3 && nextIndex > 2) {
-        if ($('.js-video-1').length) {
-          $('.js-video-1').get(0).pause();
-        }
-      }
-      if (nextIndex == 1 || nextIndex == 2) {
-        if ($('.js-video-1').length) {
-          $('.js-video-1').get(0).play();
-        }
-      }
-
-      if (index == 3) {
-        if ($('.js-video-2').length) {
-          $('.js-video-2').get(0).pause();
-        }
-      }
-      if (nextIndex == 3) {
-        if ($('.js-video-2').length) {
-          $('.js-video-2').get(0).play();
-        }
-      }
-
-      if (index == 4) {
-        if ($('.js-video-3').length) {
-          $('.js-video-3').get(0).pause();
-        }
-      }
-      if (nextIndex == 4) {
-        if ($('.js-video-3').length) {
-          $('.js-video-3').get(0).play();
-        }
-      }
-    }
+  //fix IE svg text
+  $('body').css('font-size', '16px');
+  $('body').attr({
+    style: 'font-size: 16px;'
   });
+  if ($(window).width() <= 1024) {
+    $('.js-svg-size').attr('y', '42%');
+    $('.js-svg-size-2').attr('y', '58%');
+  } else {
+    $('.one').fullpage({
+      navigation: true,
+      navigationPosition: 'left',
+      scrollOverflow: true,
+      normalScrollElements: '.navigation',
+      afterRender: function () {
+        $('.js-video-1').get(0).play();
+      },
+      onLeave: function (index, nextIndex, direction) {
+        var header = $('.js-header'),
+          svgMask = $('.js-svg-mask'),
+          svgOverlay = $('.js-svg-overlay'),
+          navigation = $('.js-navigation'),
+          nav = $('#fp-nav'),
+          footer = $('.js-footer'),
+          hamburgerBtn = $('.js-hamburger');
+
+        if (index == 1) {
+          svgMask.addClass('is-active');
+          header.addClass('is-active');
+          svgOverlay.addClass('is-active');
+          hamburgerBtn.fadeIn();
+          navigation.hide().addClass('is-mod');
+          nav.addClass('is-active');
+          footer.addClass('is-hidden');
+        }
+        if (nextIndex == 1) {
+          svgMask.removeClass('is-active');
+          header.removeClass('is-active');
+          svgOverlay.removeClass('is-active');
+          hamburgerBtn.fadeOut();
+          navigation.show().removeClass('is-mod');
+          nav.removeClass('is-active');
+          footer.removeClass('is-hidden');
+        }
+
+        if (index < 3 && nextIndex > 2) {
+          if ($('.js-video-1').length) {
+            $('.js-video-1').get(0).pause();
+          }
+        }
+        if (nextIndex == 1 || nextIndex == 2) {
+          if ($('.js-video-1').length) {
+            $('.js-video-1').get(0).play();
+          }
+        }
+
+        if (index == 3) {
+          if ($('.js-video-2').length) {
+            $('.js-video-2').get(0).pause();
+          }
+        }
+        if (nextIndex == 3) {
+          if ($('.js-video-2').length) {
+            $('.js-video-2').get(0).play();
+          }
+        }
+
+        if (index == 4) {
+          if ($('.js-video-3').length) {
+            $('.js-video-3').get(0).pause();
+          }
+        }
+        if (nextIndex == 4) {
+          if ($('.js-video-3').length) {
+            $('.js-video-3').get(0).play();
+          }
+        }
+      }
+    });
+  }
 
   (function () {
     var scrollBtn = $('.js-next-slide'),
